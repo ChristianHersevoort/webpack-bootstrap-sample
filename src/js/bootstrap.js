de8c12e3021f81e8c546b5687c67680acf5acbd0
Christian Hersevoort <christian.hersevoort@gmail.com>
2015-10-04 22:35:30 +0200

    Initial commit

---
+++ src/js/bootstrap.js
@@ -0,0 +1,15 @@
+// Include bootstrap javascript
+import {
+ affix,
+ button,
+// collapse,
+ modals,
+// scrollspy,
+ tooltip,
+ alert,
+// carousel,
+ dropdown,
+ popover,
+ tab,
+ transition
+} from 'bootstrap-sass/assets/javascripts/bootstrap';
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    // The standard entry point and output config
+    entry: {
+        bootstrap: "./src/bootstrap.js",
+	main: "./src/scss/main.scss",
+    },
+    devtool: "eval",
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: "[name].js",
+        chunkFilename: "[id].js"
+    },
+    resolve: {
+        modulesDirectories: ["node_modules"],
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+               test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+               loader: 'file'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            },
+            {
+                test: /\.scss$/,
+		loader: ExtractTextPlugin.extract("style-loader", "css!sass?outputStyle=expanded")
+            },
+            {
+		 test: /\.js$/,
+	         loaders: ['babel'],
+                 include: path.join(__dirname, 'src')
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css")
+    ]
+}
